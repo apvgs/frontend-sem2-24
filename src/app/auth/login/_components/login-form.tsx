9295e0d278fbbe4684b241dabb6a0db197f5a659
Arthur Spedine <tucogamesytthegamesof@gmail.com>
2024-11-15 15:53:29 -0300

    fix: login error message position

--- src/app/auth/login/_components/login-form.tsx
+++ src/app/auth/login/_components/login-form.tsx
@@ -39,38 +39,42 @@ export function LoginForm() {
       className='pt-12 flex flex-col gap-4'
       onSubmit={handleSubmit(handleUserLogin)}
     >
-      <div className='flex items-center border-border border-b-2 space-x-0.5 px-1.5'>
-        <span>
-          <Mail />
-        </span>
-        <input
-          type='email'
-          placeholder='e-mail'
-          className='w-full p-2 placeholder:text-muted-foreground focus-visible:outline-none'
-          {...register('email')}
-        />
+      <div className='flex flex-col space-y-1'>
+        <div className='flex space-x-0.5 items-center border-border border-b-2 px-1.5'>
+          <span>
+            <Mail />
+          </span>
+          <input
+            type='email'
+            placeholder='e-mail'
+            className='w-full p-2 placeholder:text-muted-foreground focus-visible:outline-none'
+            {...register('email')}
+          />
+        </div>
+        {formState.errors.email && (
+          <p className='text-destructive text-sm'>
+            {formState.errors.email.message}
+          </p>
+        )}
       </div>
-      {formState.errors.email && (
-        <p className='text-destructive text-sm'>
-          {formState.errors.email.message}
-        </p>
-      )}
-      <div className='flex items-center border-border border-b-2 space-x-0.5 px-1.5'>
-        <span>
-          <LockKeyhole />
-        </span>
-        <input
-          type='password'
-          placeholder='Senha'
-          className='w-full p-2 placeholder:text-muted-foreground focus-visible:outline-none'
-          {...register('password')}
-        />
+      <div className='flex flex-col space-y-1'>
+        <div className='flex space-x-0.5 items-center border-border border-b-2 px-1.5'>
+          <span>
+            <LockKeyhole />
+          </span>
+          <input
+            type='password'
+            placeholder='Senha'
+            className='w-full p-2 placeholder:text-muted-foreground focus-visible:outline-none'
+            {...register('password')}
+          />
+        </div>
+        {formState.errors.email && (
+          <p className='text-destructive text-sm'>
+            {formState.errors.email.message}
+          </p>
+        )}
       </div>
-      {formState.errors.email && (
-        <p className='text-destructive text-sm'>
-          {formState.errors.email.message}
-        </p>
-      )}
       <Button className='py-6 text-base mt-4'>Entrar</Button>
     </form>
   )
